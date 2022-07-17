4e5cd2818a553506ec274d1bda8ee14167912a8c
Nikunj <nikunjvar2002@gmail.com>
2022-07-17 11:25:44 -0700

    array

---
+++ searchin2darray.cpp
@@ -0,0 +1,54 @@
+//approach1-
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& mat, int key) {
+        int n = mat.size();
+        int m = mat[0].size();
+        if(n == 0 || m == 0)
+            return false;
+        int low = 0;
+        int high = n*m -1;
+        
+        while(low <= high)
+        {
+            int mid = low+(high-low)/2;
+            int val =  mat[mid/m][mid%m];
+            if(val == key)
+                return true;
+            if(val>key)
+                high = mid-1;
+            else 
+                low = mid+1;
+        }
+        return false;
+    }
+};
+
+//approach2-
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& mat, int key) {
+        int m=mat.size();
+        int n=mat[0].size();
+       
+        if(m==0 && n==0)
+            return false;
+        
+        int row=0,col=n-1;
+        
+        while(row < m && col >= 0){
+            int val=mat[row][col];
+            if(val == key)
+                return true;
+            else if( val > key){
+                col--;
+            }
+            else{
+                row++;
+            }
+        }
+        return false;
+    }
+};
