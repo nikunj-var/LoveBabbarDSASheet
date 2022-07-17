4c9def9113c28900add9e4f5aa141bcdb8201ceb
Nikunj <nikunjvar2002@gmail.com>
2022-07-16 19:57:03 -0700

    array

---
+++ MaxAndMinelement.cpp
@@ -0,0 +1,24 @@
+//find the sum of maximum and minimum element in the array
+
+
+class Solution
+{
+   public:
+    int findSum(int arr[], int n)
+    {   
+        int maxi=INT_MIN;
+        int mini=INT_MAX;
+    	//function to find the minimum and maximum elements
+    	for(int i=0;i<n-1;i++){
+    	    if(arr[i] < arr[i+1]){
+    	        maxi=max(arr[i+1],maxi);
+    	        mini=min(arr[i],mini);
+    	    }
+    	    else{
+    	        maxi=max(arr[i],maxi);
+    	        mini=min(arr[i+1],mini);
+    	    }
+    	}
+    	return mini+maxi;
+    }
+};
