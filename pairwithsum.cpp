e1e48825ef2c54357ed0d9b8cbc1461a305eb760
Nikunj <nikunjvar2002@gmail.com>
2022-07-25 14:40:30 -0700

    linkedlist

---
+++ pairwithsum.cpp
@@ -0,0 +1,82 @@
+// Given a sorted doubly linked list of positive distinct elements, the task is to find pairs in a doubly-linked list whose sum is equal to given value x, without using any extra space? 
+
+//creation of linked list
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+class node{
+   
+    public:
+         int data;
+        node* next=0;
+        node* prev=0;
+        node(int data){
+            this->data=data;
+        }
+};
+node* insertlinkedlist( node *head,int data){
+    
+    node *newnode;
+    newnode=new node(data);
+    if(!newnode){
+        cout<<"node not created"<<endl;
+        return head;
+    }
+    newnode->next=0;
+    if(head==NULL){
+        head=newnode;
+    }
+    else{
+        node *temp=head;
+        while(temp->next!=NULL){
+            temp=temp->next;
+        }
+        newnode->prev=temp;
+        temp->next=newnode;
+    }
+    return head;
+}
+
+void pairsum(node* head,int target){
+    node* first=head,*second=head;
+    bool found=false;
+    while(second->next != NULL){
+        second=second->next;
+    }
+    while(first != second && second->next!=first){
+        if(first->data + second->data == target){
+            cout<<endl<<first->data<<" "<<second->data<<endl;
+            found=true;
+            break;
+        }
+        else if(first->data + second->data < target){
+            first=first->next;
+        }
+        else{
+            second=second->prev;
+        }
+    }
+    if(!found)
+        cout<<"no pair exists";
+}
+
+void print(node *head){
+    node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<endl;
+        temp=temp->next;
+    }
+}
+int  main(){
+    node *head=NULL;
+    int pos;
+    head= insertlinkedlist(head,10);
+    head= insertlinkedlist(head,20);
+    head= insertlinkedlist(head,30);
+    head= insertlinkedlist(head,40);
+    head= insertlinkedlist(head,50);
+    head= insertlinkedlist(head,60);
+    pairsum(head,120);
+     print(head);
+
+}
